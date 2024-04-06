54f21ec7d3d4377b616c1557d9ad54df8999d274
Nick <nbeirne@users.noreply.github.com>
2024-04-06 16:06:47 -0400

    Fix proportion to work with negative values

--- software/src/HSUtils.cpp
+++ software/src/HSUtils.cpp
@@ -42,7 +42,7 @@ uint8_t screensaver_mode = 2; // 0 = blank, 1 = Meters, 2 = Zaps
 int Proportion(int numerator, int denominator, int max_value) {
     simfloat proportion = int2simfloat((int32_t)numerator) / (int32_t)denominator;
     int scaled = simfloat2int(proportion * max_value);
-    return scaled;
+    return scaled >= 0 ? scaled : scaled + 1;
 }
 
 /* Proportion CV values into pixels for display purposes.
